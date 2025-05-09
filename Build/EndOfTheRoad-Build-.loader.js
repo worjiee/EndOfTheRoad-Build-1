9f99da17eb1f1026c0bcdaf6bfaea95e43bb8a8e
Clyoth <sumiplorem@gmail.com>
2025-05-09 23:33:32 +0800

    v0.0.1.2

--- Build/EndOfTheRoad-Build-.loader.js
+++ Build/EndOfTheRoad-Build-.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 74038752,
+    wasmFileSize: 74822413,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
